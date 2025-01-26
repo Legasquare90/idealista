72781f6eca7e214ae756a3a710e8c4a228d1dc24
José Ángel Cuadrado Mingo <josean.cuadrado@gruposantander.com>
2025-01-26 19:23:07 +0100

    Simplify code and create ListingViewController

---
+++ Idealista/Idealista/Source/Presentation/Listing/ListingViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+final class ListingViewController: UIViewController {
+    private let tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+    }
+
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
+}
