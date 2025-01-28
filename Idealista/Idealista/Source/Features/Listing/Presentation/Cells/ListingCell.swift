896877c919a1dcade074127afdaf666e81663eba
José Ángel Cuadrado Mingo <josean.cuadrado@gruposantander.com>
2025-01-28 18:33:51 +0100

    Update design

--- Idealista/Idealista/Source/Features/Listing/Presentation/Cells/ListingCell.swift
+++ Idealista/Idealista/Source/Features/Listing/Presentation/Cells/ListingCell.swift
@@ -27,10 +27,34 @@ final class ListingCell: UITableViewCell {
         let label = UILabel()
         label.textColor = .black
         label.font = UIFont(name: Constants.Font.systemBold, size: 20)
+        label.setContentHuggingPriority(.required, for: .horizontal)
         return label
     }()
 
-    private let infoLabel: UILabel = {
+    private let parkingLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont(name: Constants.Font.systemRegular, size: 14)
+        return label
+    }()
+
+    private let sizeLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont(name: Constants.Font.systemRegular, size: 14)
+        label.setContentHuggingPriority(.required, for: .horizontal)
+        return label
+    }()
+
+    private let roomsLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont(name: Constants.Font.systemRegular, size: 14)
+        label.setContentHuggingPriority(.required, for: .horizontal)
+        return label
+    }()
+
+    private let floorLabel: UILabel = {
         let label = UILabel()
         label.textColor = .black
         label.font = UIFont(name: Constants.Font.systemRegular, size: 14)
@@ -44,10 +68,25 @@ final class ListingCell: UITableViewCell {
         return stackView
     }()
 
+    private let priceStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .lastBaseline
+        stackView.spacing = 16
+        return stackView
+    }()
+
+    private let extraInfoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 16
+        return stackView
+    }()
+
     private let labelsStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.spacing = 8
+        stackView.spacing = 10
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.layoutMargins = .init(top: 0, left: 16, bottom: 16, right: 16)
         return stackView
@@ -59,7 +98,7 @@ final class ListingCell: UITableViewCell {
         stackView.spacing = 16
         stackView.layer.borderWidth = 1
         stackView.layer.borderColor = UIColor.lightGray.cgColor
-        stackView.layer.cornerRadius = 8
+        stackView.layer.cornerRadius = 16
         stackView.clipsToBounds = true
         return stackView
     }()
@@ -76,7 +115,9 @@ final class ListingCell: UITableViewCell {
 
     private func setupCell() {
         [titleLabel, subtitleLabel].forEach(addressStackView.addArrangedSubview)
-        [addressStackView, priceLabel, infoLabel].forEach(labelsStackView.addArrangedSubview)
+        [priceLabel, parkingLabel].forEach(priceStackView.addArrangedSubview)
+        [sizeLabel, roomsLabel, floorLabel].forEach(extraInfoStackView.addArrangedSubview)
+        [addressStackView, priceStackView, extraInfoStackView].forEach(labelsStackView.addArrangedSubview)
         [thumbnailImageView, labelsStackView].forEach(contentStackView.addArrangedSubview)
         contentView.addSubview(contentStackView)
     }
@@ -85,13 +126,16 @@ final class ListingCell: UITableViewCell {
         thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
         contentStackView.translatesAutoresizingMaskIntoConstraints = false
         labelsStackView.translatesAutoresizingMaskIntoConstraints = false
+        addressStackView.translatesAutoresizingMaskIntoConstraints = false
+        priceStackView.translatesAutoresizingMaskIntoConstraints = false
+        extraInfoStackView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             contentStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
             contentStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
             contentStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             contentStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            thumbnailImageView.heightAnchor.constraint(equalToConstant: 200),
+            thumbnailImageView.heightAnchor.constraint(equalToConstant: 240)
         ])
     }
 
@@ -101,8 +145,39 @@ final class ListingCell: UITableViewCell {
         }
         titleLabel.text = viewModel.address
         subtitleLabel.text = viewModel.location
-        priceLabel.text = viewModel.price
-        infoLabel.text = viewModel.size
+        priceLabel.attributedText = getAttributedPriceText(viewModel.price)
+        sizeLabel.attributedText = getAttributedSizeText(viewModel.size)
+        roomsLabel.text = viewModel.rooms
+        floorLabel.text = viewModel.extraInfo
+
+        if let parkingInfo = viewModel.parkingInfo {
+            parkingLabel.text = parkingInfo
+            parkingLabel.isHidden = false
+        } else {
+            parkingLabel.isHidden = true
+        }
+    }
+
+    private func getAttributedPriceText(_ text: String) -> NSAttributedString {
+        let sizeText = NSMutableAttributedString(string: text)
+        let currencyText = text.split(separator: " ").last ?? ""
+        var currencyRange = (text as NSString).range(of: String(currencyText))
+        currencyRange.location -= 1
+        currencyRange.length += 1
+        if let font = UIFont(name: Constants.Font.systemBold, size: 14) {
+            sizeText.addAttribute(.font, value: font, range: currencyRange)
+        }
+        return sizeText
+    }
+
+    private func getAttributedSizeText(_ text: String) -> NSAttributedString {
+        let sizeText = NSMutableAttributedString(string: text)
+        let superIndexRange = NSRange(location: text.count - 1, length: 1)
+        sizeText.addAttribute(.baselineOffset, value: 4, range: superIndexRange)
+        if let font = UIFont(name: Constants.Font.systemRegular, size: 10) {
+            sizeText.addAttribute(.font, value: font, range: superIndexRange)
+        }
+        return sizeText
     }
 }
 
